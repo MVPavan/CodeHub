f9ea103237572c973e92e191e96a748962678914
Pavan MV <venkatapavan.muppala@jci.com>
2023-06-08 20:53:07 +0530

    zsh setup update

--- terminal_setup/zsh_tmux_setup.sh
+++ terminal_setup/zsh_tmux_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update packages and Install Git
-echo "Installing GGit..."
+echo "Updating packages and Installing Git..."
 sudo apt-get update && sudo apt-get install git
 
 # Update packages and Install ZSH
@@ -14,7 +14,8 @@ chsh -s $(which zsh)
 
 # Install Oh My Zsh
 echo "Installing Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install tmux
 sudo apt-get install tmux
@@ -25,13 +26,6 @@ sudo apt-get install fzf
 # Install fd-find
 sudo apt install fd-find
 
-# Install antigen
-curl -L git.io/antigen > ~/Downloads/terminal_setup/antigen.zsh
-
-# Setup zshrc
-cp .zshrc ~/.zshrc
-source ~/.zshrc
-
 # Install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
@@ -39,4 +33,14 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp .tmux.conf ~/.tmux.conf
 tmux source-file ~/.tmux.conf
 
+# Install antigen
+mkdir -p ~/Downloads/terminal_setup/
+curl -L git.io/antigen > ~/Downloads/terminal_setup/antigen.zsh
+
+# Setup zshrc
+cp .zshrc ~/.zshrc
+
 echo "Installation complete!"
+zsh && source ~/.zshrc
+
+
